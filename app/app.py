58d2b09664caf37884c05d1f062435e126b79599
Dongeui Hong <hong918kr@berkeley.edu>
2018-10-01 20:16:52 -0700

    fix Tae to Hayne

--- app/app.py
+++ app/app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Tae'}
+    user = {'username': 'Hayne'}
     return render_template('index.html', title='Home', user=user)
 # def hello():
 #     return "Hello World!"
 
 @app.route('/test')
 def bootstrap_index():
-    user = {'username': 'Tae'}
+    user = {'username': 'Hayne'}
     return render_template('bootstrap.html', title='Home', user=user, bootstrap=bootstrap)
 
 if __name__ == '__main__':
